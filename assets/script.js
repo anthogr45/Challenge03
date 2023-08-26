2d1198cf053e1364687dd5037789c89fa988b62d
AnthoG <anthony.dswg@gmail.com>
2023-08-26 17:29:02 -0400

    Secound commit with all validations and conditions

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,15 @@ var generateBtn = document.querySelector("#generate");
 
 function writePassword() {
 
-  var password;
+  var password = "";
   var passwordprint;
   var checkUpercase;
+  var checkLowercase;
   var checkSpChars;
   var checkNumbers;
   var counter = 0;
 
+ 
   alert ("Hello! The password has to be between 8  and 120 characters!");
   var pwLength = prompt("What should be the length of password ");
 
@@ -32,6 +34,7 @@ function writePassword() {
       if((pwLength >= 8) && (pwLength <= 120)) {
 
         checkUpercase = confirm("Do you want uppercase letters in your password ?");
+        checkLowercase = confirm("Do you want lowercase letters in your password ?");
         checkSpChars = confirm("Do you want special characters in your password ?");
         checkNumbers = confirm("Do you want numbers in your password ?");
         passwordprint();
@@ -40,31 +43,126 @@ function writePassword() {
 
         noPws(); // Type the Value for the Password to be printed
       }
-     
-   }
+    
+   } 
 
-   function passwordprint () {
+ function passwordprint () {
 
     var pwUpercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     var pwLowercase = pwUpercase.toLocaleLowerCase();
     var pwNumbers = "1234567890"
     var pwSpchars = "!@#$%^&*()_+=-`~<>/?;:\|][{}";
+    var conPwsstring;
+    var pwsString
+    var ranPws;
 
-    if (checkUpercase)
+    function Password_Generator (pwsString) {
+      for (var i = 0; i < pwLength; i++){
+        ranPws = Math.floor(Math.random() * pwsString.length);
+        password += pwsString.substring(ranPws, ranPws +1);
+       
+      }
+      console.log(password)
+    }
 
+    if ((checkUpercase) && (checkLowercase) && (checkSpChars) && (checkNumbers)) {
 
-    console.log("password Print" );
+      conPwsstring = pwUpercase.concat(pwLowercase, pwNumbers, pwSpchars);
+      Password_Generator(conPwsstring);
 
+    }else if ((!checkUpercase) && (checkLowercase) && (checkSpChars) && (checkNumbers)) {
 
+      conPwsstring = pwNumbers.concat(pwLowercase, pwSpchars);
+      Password_Generator(conPwsstring);
+    }
+
+    if((!checkUpercase) && (!checkLowercase) && (!checkSpChars) && (checkNumbers)) {
+
+      conPwsstring = pwNumbers;
+      Password_Generator(conPwsstring);
+
+    }else if ((!checkUpercase) && (!checkLowercase) && (!checkSpChars) && (!checkNumbers)) {
+      alert("The password has to be atleast of one combination type")
+      checkUpercase = confirm("Do you want uppercase letters in your password ?");
+      checkLowercase = confirm("Do you want lowercase letters in your password ?");
+      checkSpChars = confirm("Do you want special characters in your password ?");
+      checkNumbers = confirm("Do you want numbers in your password ?");
+      noCombination();
+    }
+
+    if ((checkUpercase) && (checkLowercase) && (!checkSpChars) && (!checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwLowercase);
+      Password_Generator(conPwsstring);
+
+    }else if ((checkUpercase) && (checkLowercase) &&  (!checkSpChars) && (checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwLowercase, pwNumbers);
+      Password_Generator(conPwsstring);
+    }
+
+    if ((checkUpercase) && (checkLowercase) &&  (checkSpChars) && (!checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwLowercase, pwSpchars);
+      Password_Generator(conPwsstring);
+    }else if ((!checkUpercase) && (checkLowercase) &&  (checkSpChars) && (!checkNumbers)) {
+
+      conPwsstring = pwLowercase.concat(pwSpchars);
+      Password_Generator(conPwsstring);
+    }
+    
+    if ((!checkUpercase) && (!checkLowercase) && (checkSpChars) && (checkNumbers)) {
+
+      conPwsstring = pwSpchars.concat(pwNumbers);
+      Password_Generator(conPwsstring);
+
+    }else if ((checkUpercase) && (!checkLowercase) && (!checkSpChars) && (!checkNumbers)) {
+
+      conPwsstring = pwUpercase;
+      Password_Generator(conPwsstring);
+    }
+
+    if ((checkUpercase) && (!checkLowercase) && (checkSpChars) && (checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwNumbers, pwSpchars);
+
+      Password_Generator(conPwsstring);
+
+    }else if ((checkUpercase) && (!checkLowercase) && (!checkSpChars) && (checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwNumbers);
+      Password_Generator(conPwsstring);
+    }
+
+    if ((checkUpercase) && (!checkLowercase) && (!checkSpChars) && (checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwNumbers);
+
+      Password_Generator(conPwsstring);
+
+    }else if ((checkUpercase) && (checkLowercase) && (!checkSpChars) && (!checkNumbers)) {
+
+      conPwsstring = pwUpercase.concat(pwLowercase);
+      Password_Generator(conPwsstring);
+    }
+  }
+
+   function noCombination () {
+    console.log ("No data inputed");
+    passwordprint();
    }
 
-   function noPws () {
 
-    console.log ("No data inputed");
+    var password = passwordprint;
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
 
+    return; 
+
+  
   }
- 
-}
+
 
     // var password = passwordprint;
     // var passwordText = document.querySelector("#password");
@@ -78,7 +176,7 @@ function writePassword() {
   
   
   
-  //////////---------------------------------------------------//////////////////////
+  
   
 
 // Add event listener to generate button
